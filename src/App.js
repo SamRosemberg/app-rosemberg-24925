7b5992fd4b2756ab7c1c8f90d5d51abecd1f681e
Smirosa <rosembergsamanta@gmail.com>
2022-02-08 16:39:10 -0300

    contador de stock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import { FunctionCounter} from './components/FunctionCounter/FunctionCounter'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import {NavBar} from './components/NavBar/NavBar'
 
 function App() {
+
   return (
     <>
     <NavBar />
     <ItemListContainer />
+    <FunctionCounter />
     </>
   );
 }
---
+++ src/components/FunctionCounter/FunctionCounter.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import './FunctionCounter.css'
+
+export const FunctionCounter = (props) => {
+    const [count, setCount] = useState(0)
+
+    let stock = 15;
+
+    const decrement = () => {
+        if(count > 0) {
+            setCount(count - 1)
+        }
+    }
+
+    const increment = () => {
+        if(count < stock) {
+            setCount(count + 1)
+        }
+    }
+
+    const agregarAlCarrito = () => {
+        alert('Se han agregado: ' + count + ' al carrito')
+    }
+
+    return (
+        <div className="text-center ">
+            <h3>Function Counter</h3>
+            <h3>{count}</h3>
+            <div className="col">
+                <button className="btn btn-light mx-3" disabled={count <= 0 ? true : false} onClick={decrement}>-</button>
+                <button className="btn btn-light mx-3" disabled={stock == 0 ? true : false} onClick={increment}>+</button>
+            </div>
+            <button className="btn btn-lg btn-light my-3" onClick={agregarAlCarrito}  disabled={count <= 0 ? true : false}>Agregar al carrito</button>
+        </div>
+    )
+}
+
+
